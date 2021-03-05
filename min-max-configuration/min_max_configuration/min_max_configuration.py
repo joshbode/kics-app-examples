1f3a94c7b43859df1bc4b6b8f179984b1af724d6
rjsvieira <ricardo.vieira@kelvininc.com>
2021-03-05 18:06:56 +0000

    custom-configurations-app: 1 - fixing errors in both code and app.yaml

--- min-max-configuration/min_max_configuration/min_max_configuration.py
+++ min-max-configuration/min_max_configuration/min_max_configuration.py
@@ -7,10 +7,10 @@ class App(DataApplication):
     """Application."""
 
     def process(self):
-        random_value = self.data.get('random_value')
+        temperature = self.data.get('temperature')
         min_threshold = self.config.complex_config.min_threshold
         max_threshold = self.config.complex_config.max_threshold
-        if random_value and min_threshold <= random_value <= max_threshold:
+        if temperature and min_threshold <= temperature <= max_threshold:
             print(self.config.complex_config.success_message)
         else:
             print(random.choice(self.config.complex_config.rejection_messages))
