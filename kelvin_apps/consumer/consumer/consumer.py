3a610a09c837e899086c2fcea852363a884510f2
rjsvieira <ricardo.vieira@kelvininc.com>
2021-07-28 15:52:36 +0100

    generic improvements & logging details of the app

--- kelvin_apps/consumer/consumer/consumer.py
+++ kelvin_apps/consumer/consumer/consumer.py
@@ -2,10 +2,7 @@
 Data Application.
 """
 
-from typing import Sequence
-
 from kelvin.app import DataApplication
-from kelvin.icd import Message
 from kelvin.message.raw import Float32
 from kelvin.message.raw import Int32
 
@@ -14,54 +11,41 @@ class App(DataApplication):
 
     def init(self) -> None:
         # To access the declared inputs
-        print('\nInputs: ' + str(self.interface.inputs))
+        self.logger.info("Inputs", inputs=self.interface.inputs)
         # To access the declared outputs
-        print('\nOutputs: ' + str(self.interface.outputs))
+        self.logger.info("Outputs", outputs=self.interface.outputs)
         # To access the configuration block under app->kelvin-configuration
-        print('\nConfigurations: ' + str(self.config))
+        self.logger.info("Configurations", configurations=self.config)
         # To access the complete app configuration i.e. app.yaml
-        print('\nComplete app configuration: ' + str(self.app_configuration))
-        print('\nData: ' + str(self.data))
+        self.logger.info("Complete configurations", complete_configurations=self.app_configuration)
+        self.logger.info("Data", data=self.data)
 
-    def process_data(self, data: Sequence[Message]) -> None:
-        # Process input messages
-        for msg in data:
-            print('[ Received:')
-            print('[ ' + msg._.type + ' -> timestamp: ' + str(msg._.time_of_validity * 1e-9))
-            print('[ name: ' + msg._.name)
-            print('[ value: ' + str(msg.value))
-
-            # Check for specific Inputs and generate new metrics
-            if 'temperature_in_celsius' in msg._.name:
-                temperature_in_fahrenheit = (msg.value * 9 / 5) + 32
-                if isinstance(msg, Float32):
+    def process(self) -> None:
+        try:
+            self.logger.info("Received new data: ", data=self.data)
+            temperature_in_celsius = self.data.get("temperature_in_celsius", None)
+            if temperature_in_celsius:
+                temperature_in_fahrenheit = (temperature_in_celsius.value * 9 / 5) + 32
+                if isinstance(temperature_in_celsius._.type, Float32):
                     name = "temperature_in_fahrenheit"
                     value = round(temperature_in_fahrenheit, 2)
                     self.make_message("raw.float32", name, value, emit=True)
-                elif isinstance(msg, Int32):
+                elif isinstance(temperature_in_celsius._.type, Int32):
                     name = "temperature_in_fahrenheit_int"
                     value = int(temperature_in_fahrenheit)
                     self.make_message("raw.int32", name, value, emit=True)
-                else:
-                    print('Unsupported message type: ' + str(msg.type))
-                    return
-            elif 'measure_in_cm' in msg._.name:
-                measure_in_inches = msg.value / 2.54
-                if isinstance(msg, Float32):
+
+            measure_in_cm = self.data.get("measure_in_cm", None)
+            if measure_in_cm:
+                measure_in_inches = measure_in_cm.value / 2.54
+                if isinstance(measure_in_cm._.type, Float32):
                     name = "measure_in_inches"
                     value = round(measure_in_inches, 2)
                     self.make_message("raw.float32", name, value, emit=True)
-                elif isinstance(msg, Int32):
+                elif isinstance(measure_in_cm._.type, Int32):
                     name = "measure_in_inches_int"
                     value = int(measure_in_inches)
                     self.make_message("raw.int32", name, value, emit=True)
-                else:
-                    print('Unsupported message type: ' + str(msg.type))
-                    return
-            else:
-                print('Discarding message: ' + str(msg._.name))
-                return
-
-            print('[ Published:')
-            print('[ name: ' + name)
-            print('[ value: ' + str(value))
+            self.logger.info("Dispatching new values...")
+        except Exception as exc:
+            self.logger.error("Error processing value: ", error=exc)
--- kelvin_apps/producer/producer/producer.py
+++ kelvin_apps/producer/producer/producer.py
@@ -14,22 +14,22 @@ class App(DataApplication):
 
     def init(self) -> None:
         # To access the declared inputs
-        print('\nInputs: ' + str(self.interface.inputs))
+        self.logger.info("Inputs", inputs=self.interface.inputs)
         # To access the declared outputs
-        print('\nOutputs: ' + str(self.interface.outputs))
+        self.logger.info("Outputs", outputs=self.interface.outputs)
         # To access the configuration block under app->kelvin-configuration
-        print('\nConfigurations: ' + str(self.config))
+        self.logger.info("Configurations", configurations=self.config)
         # To access the complete app configuration i.e. app.yaml
-        print('\nComplete app configuration: ' + str(self.app_configuration))
-        print('\nData: ' + str(self.data))
+        self.logger.info("Complete configurations", complete_configurations=self.app_configuration)
+        self.logger.info("Data", data=self.data)
         self.enabled = self.config.enabled
         if self.enabled:
             self.min_value = self.config.min
             self.max_value = self.config.max
             if self.min_value is None or self.max_value is None:
-                print('Missing configuration keys (min/max).')
+                self.logger.warning("Missing configuration keys (min/max).")
         else:
-            print('Data Generation is disabled.')
+            self.logger.info("Data Generation is disabled.")
 
     def process(self) -> None:
         """Process data."""
@@ -41,14 +41,10 @@ class App(DataApplication):
 
     def emit_message(self, metric, value):
         # Create message
-        msg = self.make_message(
+        message = self.make_message(
             metric.data_type,
             name=metric.name,
             value=round(value, 2)
         )
-        print('[ Published:')
-        print('[ ' + metric.data_type + ' -> timestamp: ' + str(msg._.time_of_validity * 1e-9))
-        print('[ name: ' + metric.name)
-        print('[ value: ' + str(msg.value))
-        # Emit message
-        self.emit(msg)
+        self.logger.info("Published value: ", message=str(message))
+        self.emit(message)
